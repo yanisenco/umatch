fa246ff1378502b638b118bf5c0420eaadcb12ef
Yanis <93594655+yanisenco@users.noreply.github.com>
2025-02-24 19:53:57 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: { 
+    extend: {
+      colors: {
+        white: '#FFFFFF',
+        yellow: '#FBB13C',
+        green: '#218380',
+        red: '#D81159',
+        'light-blue': '#73D2DE',
+        'dark-red': '#8F2D56',
+        black: '#101010',
+        'black-80': '#404040',
+        'black-60': '#707070',
+        'black-40': '#9F9F9F',
+        'black-20': '#CFCFCF',
+        'input-color': '#E7EFF0'
+      },
+      width: {
+        small: '16.666667%',
+        medium: '50%',
+        large: '83.333333%',
+        'x-large': '100%',
+        page: '75%',
+      },
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif'],
+      },
+    },
+  }
+}
